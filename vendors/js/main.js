d3eb1fe5848132cc65713b604fe6b54aadff367a
rawshad <rawshad@outlook.com>
2022-06-28 16:37:22 +0600

    about section added

--- vendors/js/main.js
+++ vendors/js/main.js
@@ -1,8 +1,4 @@
-/* ===================================================================
- *  Khronos 2.0.0 - Main JS
- *
- *
- * ------------------------------------------------------------------- */
+
 
 (function(html) {
 
@@ -10,258 +6,31 @@
 
     const cfg = {
 
-        // Countdown Timer Final Date
-        finalDate : 'March 20, 2024 00:00:00',
-        // MailChimp URL
-        mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
+        
 
     };
 
-
-   /* Preloader
-    * -------------------------------------------------- */
     const ssPreloader = function() {
 
-        const body = document.querySelector('body');
-        const preloader = document.querySelector('#preloader');
-        const info = document.querySelector('.s-info');
-
-        if (!(preloader && info)) return;
-
-        html.classList.add('ss-preload');
-
-        window.addEventListener('load', function() {
-
-            html.classList.remove('ss-preload');
-            html.classList.add('ss-loaded');
-
-            // page scroll position to top
-            preloader.addEventListener('transitionstart', function gotoTop(e) {
-                if (e.target.matches('#preloader')) {
-                    window.scrollTo(0, 0);
-                    preloader.removeEventListener(e.type, gotoTop);
-                }
-            });
-
-            preloader.addEventListener('transitionend', function afterTransition(e) {
-                if (e.target.matches('#preloader'))  {
-                    body.classList.add('ss-show');
-                    e.target.style.display = 'none';
-                    preloader.removeEventListener(e.type, afterTransition);
-                }
-            });
-
-        });
-
-        window.addEventListener('beforeunload' , function() {
-            body.classList.remove('ss-show');
-        });
+        
     };
 
-
-   /* Countdown Timer
-    * ------------------------------------------------------ */
     const ssCountdown = function () {
 
-        const finalDate = new Date(cfg.finalDate).getTime();
-        const daysSpan = document.querySelector('.counter .ss-days');
-        const hoursSpan = document.querySelector('.counter .ss-hours');
-        const minutesSpan = document.querySelector('.counter .ss-minutes');
-        const secondsSpan = document.querySelector('.counter .ss-seconds');
-        let timeInterval;
-
-        if (!(daysSpan && hoursSpan && minutesSpan && secondsSpan)) return;
-
-        function timer() {
-
-            const now = new Date().getTime();
-            let diff = finalDate - now;
-
-            if (diff <= 0) {
-                if (timeInterval) { 
-                    clearInterval(timeInterval);
-                }
-                return;
-            }
-
-            let days = Math.floor( diff/(1000*60*60*24) );
-            let hours = Math.floor( (diff/(1000*60*60)) % 24 );
-            let minutes = Math.floor( (diff/1000/60) % 60 );
-            let seconds = Math.floor( (diff/1000) % 60 );
-
-            if (days <= 99) {
-                if (days <= 9) {
-                    days = '00' + days;
-                } else { 
-                    days = '0' + days;
-                }
-            }
-
-            hours <= 9 ? hours = '0' + hours : hours;
-            minutes <= 9 ? minutes = '0' + minutes : minutes;
-            seconds <= 9 ? seconds = '0' + seconds : seconds;
-
-            daysSpan.textContent = days;
-            hoursSpan.textContent = hours;
-            minutesSpan.textContent = minutes;
-            secondsSpan.textContent = seconds;
-
-        }
-
-        timer();
-        timeInterval = setInterval(timer, 1000);
+        
     };
 
 
-   /* Swiper
-    * ------------------------------------------------------ */ 
     const ssSwiper = function() {
 
-        const mySwiper = new Swiper('.swiper-container', {
-
-            slidesPerView: 1,
-            effect: 'fade',
-            speed: 2000,
-            autoplay: {
-                delay: 5000,
-            }
-
-        });
+        
     };
 
 
-   /* MailChimp Form
-    * ---------------------------------------------------- */ 
+
     const ssMailChimpForm = function() {
 
-        const mcForm = document.querySelector('#mc-form');
-
-        if (!mcForm) return;
-
-        // Add novalidate attribute
-        mcForm.setAttribute('novalidate', true);
-
-        // Field validation
-        function hasError(field) {
-
-            // Don't validate submits, buttons, file and reset inputs, and disabled fields
-            if (field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return;
-
-            // Get validity
-            let validity = field.validity;
-
-            // If valid, return null
-            if (validity.valid) return;
-
-            // If field is required and empty
-            if (validity.valueMissing) return 'Please enter an email address.';
-
-            // If not the right type
-            if (validity.typeMismatch) {
-                if (field.type === 'email') return 'Please enter a valid email address.';
-            }
-
-            // If pattern doesn't match
-            if (validity.patternMismatch) {
-
-                // If pattern info is included, return custom error
-                if (field.hasAttribute('title')) return field.getAttribute('title');
-
-                // Otherwise, generic error
-                return 'Please match the requested format.';
-            }
-
-            // If all else fails, return a generic catchall error
-            return 'The value you entered for this field is invalid.';
-
-        };
-
-        // Show error message
-        function showError(field, error) {
-
-            // Get field id or name
-            let id = field.id || field.name;
-            if (!id) return;
-
-            let errorMessage = field.form.querySelector('.mc-status');
-
-            // Update error message
-            errorMessage.classList.remove('success-message');
-            errorMessage.classList.add('error-message');
-            errorMessage.innerHTML = error;
-
-        };
-
-        // Display form status (callback function for JSONP)
-        window.displayMailChimpStatus = function (data) {
-
-            // Make sure the data is in the right format and that there's a status container
-            if (!data.result || !data.msg || !mcStatus ) return;
-
-            // Update our status message
-            mcStatus.innerHTML = data.msg;
-
-            // If error, add error class
-            if (data.result === 'error') {
-                mcStatus.classList.remove('success-message');
-                mcStatus.classList.add('error-message');
-                return;
-            }
-
-            // Otherwise, add success class
-            mcStatus.classList.remove('error-message');
-            mcStatus.classList.add('success-message');
-        };
-
-        // Submit the form 
-        function submitMailChimpForm(form) {
-
-            let url = cfg.mailChimpURL;
-            let emailField = form.querySelector('#mce-EMAIL');
-            let serialize = '&' + encodeURIComponent(emailField.name) + '=' + encodeURIComponent(emailField.value);
-
-            if (url == '') return;
-
-            url = url.replace('/post?u=', '/post-json?u=');
-            url += serialize + '&c=displayMailChimpStatus';
-
-            // Create script with url and callback (if specified)
-            var ref = window.document.getElementsByTagName( 'script' )[ 0 ];
-            var script = window.document.createElement( 'script' );
-            script.src = url;
-
-            // Create global variable for the status container
-            window.mcStatus = form.querySelector('.mc-status');
-            window.mcStatus.classList.remove('error-message', 'success-message')
-            window.mcStatus.innerText = 'Submitting...';
-
-            // Insert script tag into the DOM
-            ref.parentNode.insertBefore( script, ref );
-
-            // After the script is loaded (and executed), remove it
-            script.onload = function () {
-                this.remove();
-            };
-
-        };
-
-        // Check email field on submit
-        mcForm.addEventListener('submit', function (event) {
-
-            event.preventDefault();
-
-            let emailField = event.target.querySelector('#mce-EMAIL');
-            let error = hasError(emailField);
-
-            if (error) {
-                showError(emailField, error);
-                emailField.focus();
-                return;
-            }
-
-            submitMailChimpForm(this);
-
-        }, false);
+        
     };
 
 
@@ -432,23 +201,7 @@
 
    /* Alert Boxes
     * ------------------------------------------------------ */
-    const ssAlertBoxes = function() {
-
-        const boxes = document.querySelectorAll('.alert-box');
-  
-        boxes.forEach(function(box) {
-            box.addEventListener('click', function(event) {
-                if (event.target.matches('.alert-box__close')) {
-                    event.stopPropagation();
-                    event.target.parentElement.classList.add('hideit');
-
-                    setTimeout(function(){
-                        box.style.display = 'none';
-                    }, 500)
-                }
-            });
-        })
-    };
+   
 
 
    /* Smooth Scrolling
